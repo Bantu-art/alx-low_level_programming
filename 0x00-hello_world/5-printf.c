68fd2639b8ffda572062012bb15cd90f060a9009
bantu-art <brianbaraza40@gmail.com>
2023-10-05 14:57:54 -0700

    my first printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
-   *main - Entry point of the program
-   *Return: Always 0 (Success)
-   */
+ * main - Entry point
+ * Return: always 0 (success)
+ */
 int main(void)
-{  
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
