7bae2a8e3f90540c423cd8e2cbcfec54c22138c3
bantu-art <brianbaraza40@gmail.com>
2023-10-05 15:38:46 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
