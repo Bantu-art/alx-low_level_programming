2aa7c96c8d6bdf7f32c597a138a113b69fede56a
bantu-art <brianbaraza40@gmail.com>
2023-10-06 15:57:55 -0700

    alphabet in lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
