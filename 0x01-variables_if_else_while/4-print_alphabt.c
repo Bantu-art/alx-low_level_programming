dcba1eac3d5c23308bc35e81c905130d195042d9
bantu-art <brianbaraza40@gmail.com>
2023-10-06 17:00:29 -0700

    list all alphabets except q, e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Reaturn: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
 	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		if (i != 'q' && i != 'e')
 			putchar(i);
