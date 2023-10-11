01d17984188214857d844f1c3c7d297bdf1012ad
bantu-art <brianbaraza40@gmail.com>
2023-10-11 15:59:36 -0700

    checking for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * int_isalpha - checks for alphabets whether lower r upper case
+ * int _isalpha - checks for aphabets both lower and upper  case
  * @c: function parameter
  * Return: 1 (success) or 0 (failure)
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')  || (c >= 'a' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z')  || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
