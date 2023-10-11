9269d5dafa83f37dda5bef4bc52c0ca05101f17c
bantu-art <brianbaraza40@gmail.com>
2023-10-11 16:44:20 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - gives a number its absolute form
+ * @j: function parameter
+ * Return: -j (success)
+ */
+
+int _abs(int j)
+{
+	if (j < 0)
+		return (-j);
+	else
+		return (j);
+}
