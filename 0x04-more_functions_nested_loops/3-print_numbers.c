a73226442fed52d7221f1ec2b04aeab54bffae0c
bantu-art <brianbaraza40@gmail.com>
2023-10-12 14:38:27 -0700

    printing 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function prints numbers from 0 to 9 then new line
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int a;
+	int _putchar(char a);
+
+	for (a = 0 ; a <= 9 ; a++)
+		_putchar(a + '0');
+	_putchar('\n');
+}
