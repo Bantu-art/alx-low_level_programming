d2f08487b28b0f2423c620dd5434890ed6676524
bantu-art <brianbaraza40@gmail.com>
2023-10-13 15:59:26 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (a = 1 ; a <= 100 ; a++)
 	{
-		if (a % 3 == 0)
+		if ((a % 3 == 0) && (a % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (a % 3 == 0)
 			printf("%s", "Fizz");
 		else if (a % 5 == 0)
 			printf("%s", "Buzz");
-		else if ((a % 3 == 0) && (a % 5 == 0))
-			printf("%s", "FizzBuzz");
 		else
 			printf("%d", a);
 		if (a != 100)
