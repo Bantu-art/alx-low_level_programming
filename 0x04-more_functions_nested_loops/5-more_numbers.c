46ff3b7f456a43052de8a04265ca24ad5ab8f3e0
bantu-art <brianbaraza40@gmail.com>
2023-10-13 13:33:41 -0700

    printing 1 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function function prints numbers from 0  to 14
+ * Return: 0 (success)
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+
+		for (a = 0 ; a < 10 ; a++)
+		{
+			for (b = 0 ; b <= 14 ; b++)
+			{
+				if (b > 9)
+					_putchar(b / 10 + '0');
+						_putchar(b % 10 + '0');
+			}
+			_putchar('\n');
+		}
+
+}
+
