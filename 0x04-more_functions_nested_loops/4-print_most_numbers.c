4b17fb1780ec229fce6e18336fa9d02d11fe54a5
bantu-art <brianbaraza40@gmail.com>
2023-10-13 12:54:09 -0700

    printing numbers excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/**
+ * print_most_numbers - function prints 0 to 9 but 2 to 4 excluded
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+	if (a != 2 && a != 4)
+	_putchar(a + '0');
+	}
+	_putchar('\n');
+}
