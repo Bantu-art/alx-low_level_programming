e3654f25953a48d727be506fa41907cbeedcf327
Bantu-art <brianbaraza40@gmail.com>
2023-10-17 17:13:59 -0700

    updating values

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function updates value of an int it points to t 98
+ * @n: pointer to the value
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
