53e71873e014799fcdf5d0c84860bb139de39699
Bantu-art <brianbaraza40@gmail.com>
2023-10-27 08:01:55 -0700

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function prints its name followed by new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/READ.md
@@ -0,0 +1,1 @@
+commandline arguments argc_argv
