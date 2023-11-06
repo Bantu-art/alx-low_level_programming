5c4aa741e3c8931a1f125f7ff5c011511eaee05f
Bantu-art <brianbaraza40@gmail.com>
2023-11-06 04:43:15 -0800

    ABS

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -2,17 +2,17 @@
 #include "3-function_like_macro.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
 int main(void)
 {
-	    int i;
-	        int j;
+	int i;
+	int j;
 
-		    i = ABS(-98) * 10;
-		        j = ABS(98) * 10;
-			    printf("%d, %d\n", i, j);
-			        return (0);
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
 }
