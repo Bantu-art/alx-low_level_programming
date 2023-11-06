d6a14e9c1ed102e5cf7d378f6ff45168d2ad3e91
Bantu-art <brianbaraza40@gmail.com>
2023-11-06 07:45:31 -0800

    define new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  * struct dog - Represents information about a dog
+ *   * @name: The name of the dog (a string)
+ *    * @age: The age of the dog (a float)
+ *     * @owner: The owner of the dog (a string)
+ */
+
+
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
