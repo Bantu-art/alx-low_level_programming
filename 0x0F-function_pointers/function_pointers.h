8d26148569a450738333bc0ec763bd4fba0ddfba
Bantu-art <brianbaraza40@gmail.com>
2023-11-08 03:47:04 -0800

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <string.h>
-#include <stdio.h>
-int _putchar(char c);
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
