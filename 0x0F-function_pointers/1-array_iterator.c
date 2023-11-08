63adfdf1e84380baeef805af6e228d294f8b8ac7
Bantu-art <brianbaraza40@gmail.com>
2023-11-08 03:56:22 -0800

    iterates arrays

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
