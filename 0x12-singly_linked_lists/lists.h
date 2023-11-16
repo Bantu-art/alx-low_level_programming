09ac53919d4a34283efe7394139b738080785868
Bantu-art <brianbaraza40@gmail.com>
2023-11-16 04:40:18 -0800

    prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,12 +5,22 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-int _putchar(char c);
-typedef struct list_t {
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
     char *str;
-    struct list_t *next;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
